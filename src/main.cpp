8d67077fc2840c3aa96dd68970ceab43444477fe
Devin Stanley <DEVIN.STANLEYYY@GMAIL.COM>
2025-09-02 21:07:17 -0700

    Updates for preset environment

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,16 @@
 #include "seismic_model.h"
 #include "forward_kirchhoff.h"
 #include "adjoint_kirchhoff.h"
+#include "environment_presets.h"
 
 int main(int, char**){
-    float noise = 0;
-    std::vector<int> src_cords;
-    std::vector<int> rcv_cords;
-    for (int ii = 0; ii < 15; ii++) {
-        src_cords.push_back(ii * 10);
-        rcv_cords.push_back(ii * 10);
-    }
-
-    seismic_model env(src_cords, rcv_cords, 150, 15, 15, 0.002, 10, 10, 20.0, 1000);
-
-    std::vector<std::vector<float>> coords
-    {
-        {74, 74}
-    };
-    std::vector<float> amps{ 1 };
-    env.generate_model(coords, amps, noise);
+    seismic_model env = environment_presets::generate_environment(
+        environment_presets::presets::LAYERS,
+        150,
+        15,
+        0.002,
+        10
+    );
     std::cout << "Model Generated" << std::endl;
 
     forward_kirchhoff forward(env);
